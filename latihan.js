415c3903aa9e29300199e520eb07e3074c4dd671
andra <andararamadhani125@gmail.com>
2026-06-24 08:47:01 +0800

    file selasa

---
+++ latihan.js
@@ -0,0 +1,90 @@
+// console.log("hello semua")
+// var nama = "John Doe"
+// var nomorUrut = 12
+// var quote = "hari ini adalah hari selasa"
+
+// co(nomorUrut)
+// console.log(quot)nsole.log(nama)
+// console. log
+
+// Operator Aritmatika
+var a = 5
+var b = 5
+
+var tambah = a + b
+var kurang = a-b
+var kali = a * b
+var bagi = a / b
+var moodulus = a % b
+console.log("hasil tambah",tambah)
+console.log("hasil kurang",kurang)
+console.log("hasil kali",kali)
+console.log("hasil bagi",bagi)
+console.log("hasil modulus",moodulus)
+
+
+//operator perbandingan
+console.log(1 != 1)
+console.log(1 != 2)
+console.log(1 == "1")
+console.log(1 != "1")
+console.log(1 =="2")
+console.log(1 >= 1)
+console.log(2 > 1)
+
+// Operator kodisionl
+console.log(" operator kodisional ")
+
+
+// & (AND) || (OR)
+// console.log(true || false)
+var status = "open"
+var jamBuka = 9
+
+console.log( "status" == "open" || jamBuka >= 9)
+
+// operator sting
+cosnole.log("string")
+ var word = "web program"
+ console.log(word)
+ console.log(word.length)
+console.log(word.charAt(1))
+onsole.log(`aku sedang ${kata} ${kata2}`)
+
+//console.log(word.trim()
+//console.log(word.replace)
+// console.log(word.replaceAll)
+
+// mengubah menjadi string
+// .tostring
+// string (namaVariable)
+
+// Mengubah jadu angka/Number
+// present (namaVariable)
+
+//var angka = 10
+//console.log(angka+angka)
+
+//console.log(String(angka))
+//console.log(angka.toString())
+
+//var angka = angka.toString()
+
+//console.log(angka)
+//console.log(angka+angka)
+
+
+var kata = "7"
+console.log(kata)
+
+console.log(Number(kata)
+console.log(parseInt)(kata)
+
+var angka = 1,5
+var angka = 2,7
+
+console.log(angka1)
+console.log(angka2)
+
+var angka = angka1 + angka2
+console.log(angka3)
