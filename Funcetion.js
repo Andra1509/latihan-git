dff765706c28f6bce30396b436812b948e663c14
andra <andararamadhani125@gmail.com>
2026-06-25 10:06:15 +0800

    file function

---
+++ Funcetion.js
@@ -0,0 +1,39 @@
+//contoh 2 function
+
+// function tampilkan(){
+//     console.log("andra")
+// }
+// tampilkan()
+
+
+//contoh 2
+// function munculkanangkadua() {
+//     return 2
+// }
+// var tampung = munculkanangkadua();
+// console.log(tampung)
+
+// function kalikandua(angka){
+//     return angka * 2
+// }
+// var tampung = kalikandua(2)
+// console.log(tampung)
+
+//contoh 3
+// function tampilkanangka(angkapertama, angkakedua=2){
+//     return angkapertama + angkakedua
+// }
+// console.log(tampilkanangka(5, 3))
+
+
+// var a = "saya andra"
+// function tampilkan(a){
+//     console.log("hallo", a);
+// }
+// tampilkan(a)
+
+
+var fungsiperkalian = function(angkapertama, angkakedua) {
+    return angkapertama * angkakedua
+}
+console.log(fungsiperkalian(5,5))
