01cd973cefdeaff5503a40695e7812965e5a2d54
andra <andararamadhani125@gmail.com>
2026-06-26 15:35:08 +0800

    file promis async/await

--- promis.js
+++ promis.js
@@ -1,49 +1,104 @@
-// var isMomHappy = false
+// contoh code asynchronous
+// var isMomHappy = false;
 
-// //promis
-// var willtgetnewphone = new Promise(
-//     function(resolve, reject){
-//         if(isMomHappy) {
+// promise
+// var willNewGetNewPhone = new Promise(
+//     function (resolve, reject) {
+//         if (isMomHappy) {
 //             var phone = {
-//                 brand : "samsung",
-//                 tipe : "j2 prime",
-//                 color : "black"
-//             }
-//             resolve(phone);
-//         }else{
-//             var reason = new Error("Mom is not Happy!");
-//             reject(reason)
+//                 brand: "Samsung",
+//                 color: "black"
+//             };
+//             resolve(phone); //fulfiled atau janji dipenuhi
+//         } else {
+//             var reason = new Error('mom is not happy');
+//             reject(reason);
 //         }
 //     }
-// );    
+// );
 
 // function askMom(){
-//     willtgetnewphone
-//     .then(function (terpenuhi){
-// console.log(terpenuhi);
+//     willNewGetNewPhone
+//     .then(function(fulfiled){
+//         console.log(fulfiled)
 //     })
-//     .catch(function(gagal) {
-//         console.log(gagal.message)
-//     })
+//     .catch(function(error){
+//         console.log(error.message);
+//     });
 // }
-
 // askMom()
 
 
-//asyn await
-function doAcync() {
-    return new Promise( function (resolve, reject){
-        var check = true
-        if (check){
-            resolve("anda berhasil")
-        }else{
-            reject("anda gagal")
+// contoh promise lainnya
+// function periksaDataPasien(nomorIdPasien){
+//     var dataPasien = [
+//         {id : 1, nama: "John", jenisKelamin : "Laki-Laki"},
+//         {id : 2, nama: "Michael", jenisKelamin : "Laki-Laki"},
+//         {id : 3, nama: "Sarah", jenisKelamin : "Perempuan"},
+//         {id : 4, nama: "Frank", jenisKelamin : "Laki-Laki"},
+//     ]
+//     return new Promise( function (resolve, reject){
+//         var pasien = dataPasien.find(x=> x.id === nomorIdPasien)
+//         if (pasien === undefined){
+//             reject("data pasien tidak ada")
+//         }else {
+//             resolve(pasien)
+//         }
+//     })
+// }
+
+// periksaDataPasien(5).then(function(data){
+//     console.log(data)
+// }).catch(function(err){
+//     console.log(err)
+// })
+
+
+// 
+// function doAsync(){
+//     return new Promise (function (resolve, reject){
+//         var check = false
+//         if (check){
+//             resolve("berhasil")
+//         }else{
+//             reject("gagal")
+//         }
+//     })
+// }
+// async function hello() {
+//     try{
+//         var result = await doAsync()
+//         console.log(result)
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+// hello()
+
+
+// Tugas
+function doAsync(nomorIdPasien){
+    return new Promise (function (resolve, reject){
+        var dataPasien = [
+        {id : 1, nama: "John", jenisKelamin : "Laki-Laki"},
+        {id : 2, nama: "Michael", jenisKelamin : "Laki-Laki"},
+        {id : 3, nama: "Sarah", jenisKelamin : "Perempuan"},
+        {id : 4, nama: "Frank", jenisKelamin : "Laki-Laki"},
+        ]
+        var pasien = dataPasien.find(x=> x.id === nomorIdPasien)
+        if (pasien === undefined){
+            reject("data pasien tidak ada")
+        }else {
+            resolve(pasien)
         }
     })
 }
-
-async function hello() {
-    var result = await doAcync()
-    console.log(result)
+async function hello(nomorIdPasien) {
+    try{
+        var result = await doAsync(nomorIdPasien)
+        console.log(result)
+    }catch(err){
+        console.log(err)
+    }
 }
-hello()
+hello(1)
