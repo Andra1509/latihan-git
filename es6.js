877d2f27c0a5e4a693695d151c82ef15b2b5e544
andra <andararamadhani125@gmail.com>
2026-06-26 10:42:05 +0800

    fiele prommis

--- es6.js
+++ es6.js
@@ -49,11 +49,99 @@
 // HitungLuasPersegi(6)
 
 
-let nama = "tri agil pribadi"
-console.log("hallo selamat datang.",nama)
-console.log("hallo selamat datang. "+ nama)
+// let nama = "tri agil pribadi"
+// console.log("hallo selamat datang.",nama)
+// console.log("hallo selamat datang. "+ nama)
 
-console.log("hallo, "+nama +"!. selamat datang.")
+// console.log("hallo, "+nama +"!. selamat datang.")
 
-console.log(`hallo, $(nama). selamat datang!`)
+// console.log(`hallo, $(nama). selamat datang!`)
 
+// text1 = "hallo"
+// text2 = "selamat"
+// text3 = "datang"
+ 
+// console.log("text1+")
+
+//
+// let nama = "Andra Ramadhani Widodo"
+// let person = {
+//     nama : nama,
+//     umur : 17 
+// }
+// console.log(person)
+
+// //destructering array
+// let number = [1,5,6,7,8]
+
+// let num1 = number[0]
+// let num2 = number[1]
+// let num3 = number[2]
+// let num4 = number[3]
+// let num5 = number[4]
+
+// let [num1, num2, num3, num4, lastnum] = [1,5,6,7,8]
+
+// console.log(lastnum)
+
+//destructering object
+
+// let names = "Andra Ramadhani Widodo"
+//  let person = {
+//      nama : names,
+//      umur : 17,
+//      tinggi : 174
+//  }
+
+// //  let namaperson = person.nama
+// //  let umurperson = person.umur
+// //  let tinggiperson = person.tinggi
+// let {nama, umur, tinggi} = person
+//  console.log(person)
+
+
+ //rest parameter array
+//  let [num1, num2, num3, num4, lastnum] = [1,5,6,7,8]
+
+//  let fotballers = ["messi", "ronaldo", "mbappe", "halland", "neymar"]
+//  let [foot1, foot2,...restfoot] = fotballers
+
+//  console.log(foot2)
+//  console.log(foot1)
+//  console.log(restfoot)
+
+//  rest parameter array
+//   let names = "Andra Ramadhani Widodo"
+//   let person = {
+//       nama : names,
+//       umur : 17,
+//       tinggi : 173
+//   }
+
+//       let {nama, ...restperson} = person
+//       console.log(nama)
+//       console.log(restperson)
+
+    //spread operator
+
+    // let buah = ["strauberry", "mangga", "apel"]
+
+    // buah.unshift("mangga")
+    // buah.push('durian')
+    // console.log(buah)
+      
+    // buah = ["mangga", ...buah, "jeruk",]
+    // console.log(buah)
+
+    let botol = {
+        merk : "aqua",
+        ukuran : "500liter",
+        harga : 5000
+    }
+    
+    // botol.warna = "biru"
+    // botol.kemasan = "plastik"
+    // botol.bentuk = "bulat"
+     
+    botol = {...botol, warna: "biru", kemasan: "plastik", bentuk: "bulat"}
+    console.log(botol)
